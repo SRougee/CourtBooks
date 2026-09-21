af2534f3901d4f2a915804761bb0861f396bc93a
SRougee <128219983+SRougee@users.noreply.github.com>
2026-09-21 22:30:58 +0200

    feat: extend payment history details

--- CourtBooks.Core/Models/InvoicePayment.cs
+++ CourtBooks.Core/Models/InvoicePayment.cs
@@ -6,4 +6,6 @@ public sealed class InvoicePayment
     public int InvoiceId { get; init; }
     public decimal Amount { get; init; }
     public DateTime PaidOn { get; init; }
+    public PaymentMethod Method { get; init; } = PaymentMethod.Other;
+    public string Reference { get; init; } = "";
 }
